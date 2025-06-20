6877514e5205111666854c5301892b4bf3c9d56f
slalom0914 <slalom0914@gmail.com>
2025-06-20 09:25:40 +0900

    서블릿설정추가

--- src/main/java/com/example/demo/basic/ServletLifeCycle.java
+++ src/main/java/com/example/demo/basic/ServletLifeCycle.java
@@ -15,7 +15,7 @@ public class ServletLifeCycle extends HttpServlet{
   @Override
   public void init() throws ServletException {
     // 서블릿 초기화 - 서블릿 생성 또는 리로딩 때 단 한번만 수행
-    log.info("ServletLifeCycle init");
+    log.info("ServletLifeCycle 초기화");
   }
   //URL요청이 있을 때 마다 반복적으로 수행됨
   @Override
@@ -24,7 +24,13 @@ public class ServletLifeCycle extends HttpServlet{
     //1. 입력
     //2. 처리
     //3. 출력
-
+    
+    // 응답 설정
+    resp.setContentType("text/html; charset=UTF-8");
+    resp.getWriter().println("<html><body>");
+    resp.getWriter().println("<h1>Hello from Servlet!</h1>");
+    resp.getWriter().println("<p>서블릿이 정상적으로 작동하고 있습니다.</p>");
+    resp.getWriter().println("</body></html>");
   }
   //뒷 정리 작업 - 서블릿이 제거될 때 단 한 번만 수행됨
   @Override
